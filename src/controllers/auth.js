e88fdb94be050b92940cfb03f8ba79ed1a3c45eb
rider <riderjensen@gmail.com>
2020-01-20 21:43:11 -0700

    auth add in token

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -16,7 +16,11 @@ exports.signup = async (req, res, next) => {
 			username: username
 		});
 		user.save().then(result => {
-			res.status(201).json({ message: 'User created!', userId: result._id })
+			const token = jwt.sign({
+				username: user.username,
+				userId: result._id.toString()
+			}, 'ZORmyTNgrMCClPb6rPuX', { expiresIn: '1d' });
+			res.status(201).json({ message: 'User created!', token: token, userId: result._id })
 		})
 	} catch (err) {
 		if (!err.statusCode) {
@@ -38,7 +42,6 @@ exports.login = async (req, res, next) => {
 			error.statusCode = 401;
 			throw error;
 		}
-		let loadedUser = user;
 		const isEqual = await bcrypt.compare(password, user.password);
 		if (!isEqual) {
 			const error = new Error('Wrong password');
@@ -46,10 +49,10 @@ exports.login = async (req, res, next) => {
 			throw error;
 		}
 		const token = jwt.sign({
-			username: loadedUser.username,
-			userId: loadedUser._id.toString()
+			username: user.username,
+			userId: user._id.toString()
 		}, 'ZORmyTNgrMCClPb6rPuX', { expiresIn: '1d' });
-		res.status(200).json({ token: token, userId: loadedUser._id.toString() });
+		res.status(200).json({ token: token, userId: user._id.toString() });
 	} catch (err) {
 		if (!err.statusCode) {
 			err.statusCode = 500;
