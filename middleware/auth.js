50fd69ea47e2b3ebcf9fa63f802498941b726d70
rider <riderjensen@gmail.com>
2020-01-22 20:46:24 -0700

    add a function for jwt

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,21 +4,19 @@ module.exports = (req, res, next) => {
 	const authHeader = req.get('Authorization');
 	if (!authHeader) {
 		req.isAuth = false;
-		return next({ error: true, message: 'Not authorized'});
+		return res.status(401).json({ error: true, message: 'Missing Auth Header' })
 	}
-	const token = authHeader.split(' ')[1];
-	const decodedToken;
-	try {
-		decodedToken = jwt.verify(token, 'ZORmyTNgrMCClPb6rPuX');
-	} catch (err) {
-		req.isAuth = false;
-		return next({ error: true, message: 'Not authorized'});
-	}
-	if (!decodedToken) {
-		req.isAuth = false;
-		return next({ error: true, message: 'Not authorized'});
-	}
-	req.userId = decodedToken.userId;
-	req.isAuth = true;
-	next();
+	jwt.verify(authHeader, 'ZORmyTNgrMCClPb6rPuX', function(err, decodedToken) {
+		if (err) {
+			req.isAuth = false;
+			return res.status(401).json({ error: true, message: 'Error decoding token' })
+		}
+		if (!decodedToken) {
+			req.isAuth = false;
+			return res.status(401).json({ error: true, message: 'Decoded token invalid' })
+		}
+		req.userId = decodedToken.userId;
+		req.isAuth = true;
+		next();
+	})
 }
