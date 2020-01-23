624de92d2291f45d98e31908919d2154f8ab195c
rider <riderjensen@gmail.com>
2020-01-22 20:25:31 -0700

    return errors for no auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,19 +4,19 @@ module.exports = (req, res, next) => {
 	const authHeader = req.get('Authorization');
 	if (!authHeader) {
 		req.isAuth = false;
-		return next();
+		return next({ error: true, message: 'Not authorized'});
 	}
 	const token = authHeader.split(' ')[1];
-	let decodedToken;
+	const decodedToken;
 	try {
 		decodedToken = jwt.verify(token, 'ZORmyTNgrMCClPb6rPuX');
 	} catch (err) {
 		req.isAuth = false;
-		return next();
+		return next({ error: true, message: 'Not authorized'});
 	}
 	if (!decodedToken) {
 		req.isAuth = false;
-		return next();
+		return next({ error: true, message: 'Not authorized'});
 	}
 	req.userId = decodedToken.userId;
 	req.isAuth = true;
