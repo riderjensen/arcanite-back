5b40ac2eb412aee4daceda9f9080d4991642fd41
rider <riderjensen@gmail.com>
2020-01-22 20:56:34 -0700

    Remove moved routes

--- src/routes/post.js
+++ src/routes/post.js
@@ -4,10 +4,8 @@ const router = express.Router();
 
 const controller = require('../controllers/post');
 
-router.route('/').get(controller.getPosts);
 router.route('/').post(controller.addPost);
 
-router.route('/:id').get(controller.getOnePost);
 router.route('/:id').post(controller.votePost);
 router.route('/:id').patch(controller.editPost);
 
