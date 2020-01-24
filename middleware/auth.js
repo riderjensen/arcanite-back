491ff101c3169b3e8f571694c2bed057fd3fe50f
rider <riderjensen@gmail.com>
2020-01-24 13:28:40 -0700

    remove jwt secret

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,12 +1,14 @@
 const jwt = require('jsonwebtoken');
 
+const { jwtsecret } = require('../../env').env;
+
 module.exports = (req, res, next) => {
 	const authHeader = req.get('Authorization');
 	if (!authHeader) {
 		req.isAuth = false;
 		return res.status(401).json({ error: true, message: 'Missing Auth Header' })
 	}
-	jwt.verify(authHeader, 'ZORmyTNgrMCClPb6rPuX', function(err, decodedToken) {
+	jwt.verify(authHeader, jwtsecret, function(err, decodedToken) {
 		if (err) {
 			req.isAuth = false;
 			return res.status(401).json({ error: true, message: 'Error decoding token, try logging in again' })
