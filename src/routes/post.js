11130f42cf23078265058189ba625c965d82fe8e
rider <riderjensen@gmail.com>
2020-01-26 18:38:21 -0700

    get one post route

--- src/routes/post.js
+++ src/routes/post.js
@@ -7,6 +7,7 @@ const controller = require('../controllers/post');
 router.route('/').post(controller.addPost);
 router.route('/all').get(controller.getUserPostsAndComments);
 router.route('/v/:id').get(controller.votePost);
+router.route('/:id').get(controller.getOnePost);
 router.route('/:id').patch(controller.editPost);
 router.route('/:id').delete(controller.deletePost);
 
